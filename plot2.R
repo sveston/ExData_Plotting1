1308ce632e889de106fb84a4d081a46a18428073
sveston <sveston@gmail.com>
2014-05-12 00:34:39 +0800

    third commit

--- plot2.R
+++ plot2.R
@@ -1,7 +1,16 @@
+##Read the entire table from folder
 hpc <- read.table("E:/R/exdata-data-household_power_consumption/hpc.txt", sep=";", quote="\"",header=TRUE)
+##Convert the Dates to Date Class
 hpc[,"Date"]<-as.Date(hpc[,"Date"],format='%d/%m/%Y')
+##Subset Dates falling on 2007-02-01 and 2007-02-02
 data<-subset(hpc,hpc$Date=="2007-02-01"|hpc$Date=="2007-02-02")
+##Change the class of Global active power from factor to numeric
 data[,"Global_active_power"]<-as.numeric(data[,"Global_active_power"])
+##Concatenate the Date and Time and form another column call DateTime
 data$DateTime <- do.call(paste,c(data[c("Date","Time")],sep=" "))
+##Convert the DateTime column to Date/Time class
 data[,"DateTime"]<-as.POSIXct(data[,"DateTime"], format = "%Y-%m-%d %H:%M:%S")
+##Set the margin of the plot
+par(mar=c(4,4,1,1))
+##Plot 
 plot(data$DateTime,data$Global_active_power/1000,lwd=1,type="l",xlab="",ylab="Global Active Power (kilowatts)")
--- plot4.R
+++ plot4.R
@@ -4,23 +4,25 @@ hpc <- read.table("E:/R/exdata-data-household_power_consumption/hpc.txt", sep=";
 hpc[,"Date"]<-as.Date(hpc[,"Date"],format='%d/%m/%Y')
 ##Subset Dates falling on 2007-02-01 and 2007-02-02
 data<-subset(hpc,hpc$Date=="2007-02-01"|hpc$Date=="2007-02-02")
-##Change the class of Sub Meterings from factor to numeric
+##Change the class of the respective headers from factor to numeric
+data[,"Global_active_power"]<-as.numeric(data[,"Global_active_power"])
+data[,"Global_reactive_power"]<-as.numeric(data[,"Global_reactive_power"])
+data[,"Voltage"]<-as.numeric(data[,"Voltage"])
 data[,"Sub_metering_1"]<-as.numeric(data[,"Sub_metering_1"])
 data[,"Sub_metering_2"]<-as.numeric(data[,"Sub_metering_2"])
 data[,"Sub_metering_3"]<-as.numeric(data[,"Sub_metering_3"])
-##Concatenate the Date and Time and form another column call DateTime
 data$DateTime <- do.call(paste,c(data[c("Date","Time")],sep=" "))
-##Convert the DateTime column to Date/Time class
 data[,"DateTime"]<-as.POSIXct(data[,"DateTime"], format = "%Y-%m-%d %H:%M:%S")
 
 ##Set the margin of the plot
 par(mar=c(4,4,1,1))
-##Plot the first Sub meter
+##Divide the canvas to fit 4 plots
+par(mfrow=c(2,2))
+##Plot the 4 plots
+plot(data$DateTime,data$Global_active_power/1000,lwd=1,type="l",xlab="",ylab="Global Active Power")
+plot(data$DateTime,data$Voltage,lwd=1,type="l",xlab="datetime",ylab="Voltage")
 plot(data$DateTime,data$Sub_metering_1,ylim=c(0,35),lwd=1,type="l",xlab="",ylab="Energy sub metering")
-##Overlay the second Sub meter
 lines(data$DateTime,data$Sub_metering_2,lwd=1,type="l",xlab="",ylab="Energy sub metering",col="red")
-##Overlay the third Sub meter
 lines(data$DateTime,data$Sub_metering_3,lwd=1,type="l",xlab="",ylab="Energy sub metering",col="blue")
-##Add the Legend
-legend("topright",pch="",lwd=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-
+legend("topright",pch="",lwd=1,bty="n",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+plot(data$DateTime,data$Global_reactive_power,lwd=1,type="l",xlab="datetime",ylab="Global_reactive_power")
