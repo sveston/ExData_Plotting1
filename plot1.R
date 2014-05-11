96432c7598d16027ed514290a882f3fd15a742bd
sveston <sveston@gmail.com>
2014-05-12 00:37:15 +0800

    third commit

--- plot1.R
+++ plot1.R
@@ -1,6 +1,14 @@
+##Read the entire table from folder
 hpc <- read.table("E:/R/exdata-data-household_power_consumption/hpc.txt", sep=";", quote="\"",header=TRUE)
+##Convert the Dates to Date Class
 hpc[,"Date"]<-as.Date(hpc[,"Date"],format='%d/%m/%Y')
+##Subset Dates falling on 2007-02-01 and 2007-02-02
 data<-subset(hpc,hpc$Date=="2007-02-01"|hpc$Date=="2007-02-02")
+##Change the class of Global active power from factor to numeric
 data[,"Global_active_power"]<-as.numeric(data[,"Global_active_power"])
+
+##Set the margin of the plot
+par(mar=c(4,4,1,1))
+##Histogram plot of Global Active Power in Kilowatts
 hist(data$Global_active_power/1000,col="red",xlab="Global Active Power (kilowatts)", main="Global Active Power")
 
